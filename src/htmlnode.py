a6cac9ed76feb70e13b7e4d965fcf73df0cab458
HactuallyBenji <mrbenamiller@gmail.com>
2024-03-07 20:46:14 -0800

    Add some HTMLNode tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,7 +12,8 @@ class HTMLNode:
     def props_to_html(self):
         html = ""
         for attribute, value in self.props.items():
-            html += f" {attribute}={value}"
+            html += f" {attribute}=\"{value}\""
+        return html
 
     def __repr__(self):
         return f"<{self.tag}> with value: {self.value}, children: {self.children}, and props: {self.props}"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,17 @@
+import unittest
+
+from htmlnode import HTMLNode 
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(tag="a", value="Click me!", props={"href": "a.com"}) 
+        expected = " href=\"a.com\""
+        self.assertEqual(expected, node.props_to_html())
+
+    def test_repr(self):
+        node = HTMLNode(tag="a", value="Click me!", props={"href": "a.com"}) 
+        expected = f"<{node.tag}> with value: {node.value}, children: {node.children}, and props: {node.props}"
+        self.assertEqual(expected, repr(node))
+
+if __name__ == "__main__":
+    unittest.main()
