86f50bfae28322bb1d2b88e7e344a33aa8d5f1f3
HactuallyBenji <mrbenamiller@gmail.com>
2024-03-07 20:48:50 -0800

    Just one more test with more than one prop

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -8,6 +8,11 @@ class TestHTMLNode(unittest.TestCase):
         expected = " href=\"a.com\""
         self.assertEqual(expected, node.props_to_html())
 
+    def test_props_to_html_longer(self):
+        node = HTMLNode(tag="a", value="Click me!", props={"class": "greeting", "href": "a.com"}) 
+        expected = " class=\"greeting\" href=\"a.com\""
+        self.assertEqual(expected, node.props_to_html())
+
     def test_repr(self):
         node = HTMLNode(tag="a", value="Click me!", props={"href": "a.com"}) 
         expected = f"<{node.tag}> with value: {node.value}, children: {node.children}, and props: {node.props}"
